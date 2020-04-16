6d2a7a185c19a65e73d0093d11b5a925ce7fabf1
bende47 <bendejustin@gmail.com>
2020-04-17 00:12:03 +0200

    Ajout de la methode de suppression de User

--- src/main/java/com/buddy/controller/UserController.java
+++ src/main/java/com/buddy/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.buddy.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +19,11 @@ public class UserController {
 	@PostMapping(value="/addUser")
 	public User addUser(@RequestBody User user) {
 		return userMetier.addUser(user);
+	}
+	
+	@GetMapping(value="/deleteUser/{iduser}")
+	public User deleteUser(@PathVariable Long iduser) {
+		return userMetier.deleteUser(iduser);
 	}
 	
 }
