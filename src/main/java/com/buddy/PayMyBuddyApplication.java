a825796a1803cb3d3c33cc4871cd025fe1c9132a
bende47 <bendejustin@gmail.com>
2020-05-01 13:47:06 +0200

    Securité de connexion à la base de données

--- src/main/java/com/buddy/PayMyBuddyApplication.java
+++ src/main/java/com/buddy/PayMyBuddyApplication.java
@@ -1,23 +1,17 @@
 package com.buddy;
 
-import javax.persistence.EntityManagerFactory;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EncryptablePropertySource;
 
 @SpringBootApplication
 @EnableJpaRepositories
-public class PayMyBuddyApplication {
-	
-	@Bean
-    JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactory);
-        return transactionManager;
-    }
+@Configuration
+@EncryptablePropertySource(name = "EncryptedProperties", value = "classpath:application.properties")
+public class PayMyBuddyApplication {	
 
 	public static void main(String[] args) {
 		SpringApplication.run(PayMyBuddyApplication.class, args);
